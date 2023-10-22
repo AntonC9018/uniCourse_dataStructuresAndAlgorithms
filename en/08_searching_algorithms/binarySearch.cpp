f02b3dc8d95899006964c9ed97c52eedf05556f7
AntonC9018 <anton.curm@gmail.com>
2023-10-22 17:49:32 +0300

    Fix underflow bug

--- en/08_searching_algorithms/binarySearch.cpp
+++ en/08_searching_algorithms/binarySearch.cpp
@@ -1,4 +1,5 @@
 #include <span>
+#include <array>
 #include <iostream>
 
 struct SearchResult
@@ -16,7 +17,7 @@ struct SearchResult
 // | 1 | 3 | 6 | 9 | 15 | 25 |
 //               ^   |     ^
 //
-// | 1 | 3 | 6 | 10 | 15 | 25 |    3
+// | 1 | 3 | 6 | 9 | 15 | 25 |    3
 //               ^
 //               ^
 //
@@ -25,8 +26,25 @@ struct SearchResult
 
 SearchResult binarySearch(std::span<int> arr, int elementToFind)
 {
-    if (arr.size() == 0)
+    if (arr.empty())
+    {
         return { 0, false };
+    }
+
+    // This is to account for the case where it would *underflow*.
+    //
+    // It would underflow when we try to move the high index to the left of the 0th position,
+    // which would take it to -1, which is also size_t's max value,
+    // so it will make the check pass, even though 
+    // the high index is now in front of the low index.
+    //
+    // You can try and debug the application without this check, and try and search for
+    // an element that is smaller than the first element in the array, 
+    // to see for youself what I mean.
+    if (arr[0] < elementToFind)
+    {
+        return { 0, false };
+    }
 
     size_t indexLow = 0;
     size_t indexHigh = arr.size() - 1;
@@ -57,7 +75,7 @@ SearchResult binarySearch(std::span<int> arr, int elementToFind)
 int main()
 {
     std::array arr = { 1, 5, 7, 10, 15, 32, 89 };
-    SearchResult result = binarySearch(arr, 89);
+    SearchResult result = binarySearch(arr, 0);
 
     if (result.isElementFound)
     {
@@ -65,7 +83,7 @@ int main()
     }
     else
     {
-        std::cout << "Elemenet not found.";
+        std::cout << "Element not found.";
     }
 
     return 0;
