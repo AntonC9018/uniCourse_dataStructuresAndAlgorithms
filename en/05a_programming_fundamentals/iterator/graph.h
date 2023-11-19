7336dfb0c82e528a95254c21e54cbedae9710fcf
AntonC9018 <anton.curm@gmail.com>
2023-11-19 02:47:02 +0200

    We're using the this-> convention

--- en/05a_programming_fundamentals/iterator/graph.h
+++ en/05a_programming_fundamentals/iterator/graph.h
@@ -10,7 +10,7 @@ struct Node
     {
         for (size_t i = 0; i < this->children.size(); i++)
             delete this->children[i];
-        children.~vector();
+        this->children.~vector();
     }
 };
 
